0b9360ac7f6a31458f8a0920d15b75849bd44b03
Your Name <sherronpowell@yahoo.com>
2019-12-17 01:27:36 -0500

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :cars
+  root 'cars#index'
+  
+  root 'cars#index'
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
